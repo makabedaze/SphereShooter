1434c0af8b142e975c3c622e4e5376c6cfd349ec
makabe <hiroaki.makabe714@gamil.com>
2019-01-28 02:55:57 +0900

    球面上の移動を仮実装。

---
+++ Other/TransformExtension.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public static class TransformExtension
+{
+	//仰角をz、方位角をx、半径をyとする
+	public static void SetSphericalPosition(this Transform transform,Vector3 targetPos)
+	{
+		var radius = targetPos.y;//半径
+		var azimuth = targetPos.x;//方位角
+		var elevation = targetPos.z;//仰角
+		transform.position = ConvertSphericalCoordinatesToCartesianCoordinates(radius, azimuth, elevation);
+	}
+
+	public static Vector3 GetSphericalPosition(this Transform transform)
+	{
+		var radius = transform.position.magnitude;
+		var azimuth = Mathf.Atan2(transform.position.z, transform.position.x);
+		var elevation = Mathf.Asin(transform.position.y / radius);
+		return new Vector3(azimuth, radius, elevation);
+	}
+
+	public static void SphericalTranslate(this Transform transform, Vector3 targetPos)
+	{
+		var radius = targetPos.x;//半径
+		var azimuth = targetPos.y;//方位角
+		var elevation = targetPos.z;//仰角
+		transform.SetSphericalPosition(transform.GetSphericalPosition() + new Vector3(radius, azimuth, elevation));
+		transform.LookAt(Vector3.zero);
+	}
+
+	public static Vector3 ConvertSphericalCoordinatesToCartesianCoordinates(float radius,float azimuth,float elevation) 
+	{
+		float t = radius * Mathf.Cos(elevation);
+		return new Vector3(t * Mathf.Cos(azimuth), radius * Mathf.Sin(elevation), t * Mathf.Sin(azimuth));
+	}
+}
--- Player/PlayerMove.cs
+++ Player/PlayerMove.cs
@@ -6,11 +6,18 @@ public class PlayerMove : MonoBehaviour
 	[SerializeField]
 	private float _speed = 0.5f;
 	private Transform _transform;
+	[SerializeField]
+	float x = 0;
+	[SerializeField]
+	float y = 0;
+	[SerializeField]
+	float z = 0;
 
 	private void Awake()
 	{
 		_input = GameObject.FindWithTag(Constants.INPUT).GetComponent<IInput>();
 		_transform = transform;
+		_transform.SetSphericalPosition(new Vector3(0,30,0));
 	}
 
 	private void Update()
@@ -20,7 +27,7 @@ public class PlayerMove : MonoBehaviour
 
 	public void Move(Vector3 inputValue)
 	{
-		_transform.position += inputValue.normalized * _speed * Time.deltaTime;
+		_transform.SphericalTranslate(inputValue.normalized * _speed);
 	}
 
 	private void Turn(Vector3 inputValue)
